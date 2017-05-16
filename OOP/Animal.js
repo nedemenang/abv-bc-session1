e7ad43cc6d639af312c37483fee24960a9871198
ABS User <ABS User>
2017-05-16 16:22:11 +0100

    Create Animal class

--- OOP/Animal.js
+++ OOP/Animal.js
@@ -1,64 +1,68 @@
 
-module.exports = {
-//computeAverage: (num1, num2, num3) => {
-
-	aritGeo: (array) =>
-	{
-
-		if(array.length < 1) return 0;
-
-		let difference = array[1] - array[0];
-		let ratio = array[1] / array[0];
-
-		let isArithmetic = true;
-		let isGeometric = true;
-
-		for(let i = 0; i < array.length - 1; i++)
-		{
-		    if( array[i + 1] - array[i] !== difference )
-		      isArithmetic = false;
-		    if(array[i + 1] / ratio !== array[i])
-		      isGeometric = false;
-		}
-
-		if(isArithmetic === true)
-		    return "Arithmetic";
-		else if(isGeometric === true)
-		    return"Geometric";
-		else
-		    return -1;
-	},
-
-	findMinMax: (array) =>
-	{
-		if(array.length < 1) return 0;
-
-		  let minValue = array[0];
-		  let maxValue = array[0];
-		  let outputArray = [];
-		  for (let i = 0; i < array.length; i++) {
-		        if (array[i] < minValue) 
-		        {
-		            minValue = array[i];
-		        }
-		        
-		        if(array[i] > maxValue)
-		        {
-		          maxValue = array[i];
-		        }
-		    }
-		    
-		    if(minValue === maxValue)
-		    {
-		      outputArray.push(minValue);
-		    }
-		    else
-		    {
-		      
-		      outputArray.push(minValue);
-		      outputArray.push(maxValue);
-		    }
-		    return outputArray;
-	}
+function Animal(name) {
+   this.name = name;
 }
 
+Animal.prototype.getName = function() {
+    return this.name;
+}
+
+function Mammal(name, hasHair) {
+    Animal.call(this, name);
+
+    this.hasHair = hasHair;
+}
+
+Mammal.prototype = Object.create(Animal.prototype);
+
+Mammal.prototype.constructor = Mammal;
+
+Mammal.prototype.getHasHair = function() {
+    return this.hasHair;
+}
+
+function Dog(name, breed) {
+    Mammal.call(this, name, true);
+
+    this.breed = breed;
+}
+
+function Human(name, race){
+  Mammal.call(this, name, true)
+  
+  this.race = race;
+}
+
+Dog.prototype = Object.create(Mammal.prototype);
+Human.prototype = Object.create(Mammal.prototype);
+
+
+
+Dog.prototype.constructor = Dog;
+Human.prototype.constructor = Human;
+
+Dog.prototype.getBreed = function() {
+    return this.breed;
+}
+
+Dog.prototype.bark = function() {
+    return "wo wo wo";
+}
+
+Human.prototype.getRace = function() {
+  return this.race;
+}
+
+Human.prototype.sayHi = function(){
+  return "Hi"
+}
+
+let nina = new Human('Nina', 'African');
+let bingo = new Dog('Bingo', 'Mongrel')
+
+nina.sayHi();  
+//nina.getHasHair();
+bingo.bark();
+ 
+//nina.getHasHair();
+//bingo.getBreed();
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # abv-bc-session1
 Andela Boot camp day 2
+
+
+Asymtotic Analysis for both exercises = O(N), N being the lenght of both arrays.
