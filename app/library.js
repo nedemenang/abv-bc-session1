9adcf40dbc352574fbd1c84d257aeab3bb42cc8e
ABS User <ABS User>
2017-05-16 12:51:07 +0100

    Rewrite isArit function, pass all test

--- app/library.js
+++ app/library.js
@@ -2,10 +2,31 @@
 module.exports = {
 //computeAverage: (num1, num2, num3) => {
 
-	arithGeo: (array) => 
+	aritGeo: (array) =>
 	{
-	
-	
+
+		if(array.length < 1) return 0;
+
+		let difference = array[1] - array[0];
+		let ratio = array[1] / array[0];
+
+		let isArithmetic = true;
+		let isGeometric = true;
+
+		for(let i = 0; i < array.length - 1; i++)
+		{
+		    if( array[i + 1] - array[i] !== difference )
+		      isArithmetic = false;
+		    if(array[i + 1] / ratio !== array[i])
+		      isGeometric = false;
+		}
+
+		if(isArithmetic === true)
+		    return "Arithmetic";
+		else if(isGeometric === true)
+		    return"Geometric";
+		else
+		    return -1;
 	}
 }
 
